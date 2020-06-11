a90f5be572d35a9684049c4a46790e0b49b74f12
gracejansen227 <graceowensjansen@gmail.com>
2020-06-11 18:18:58 -0500

    Changed to scss, added colors and toggling on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
   title = 'pride-bingo';
   data = [];
   prideTitle = [];
+  isBoxChecked = false;
 
   ngOnInit() {
     this.data = Array.from(Array(25).keys());
     this.prideTitle = ('PRIDE').split('');
   }
+
+  // checkBox(){
+  //   // fix this to make a toggle
+  //   this.isBoxChecked = !this.isBoxChecked;
+  // }
 }
