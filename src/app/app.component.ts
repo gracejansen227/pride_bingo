4c34900e90ce99f7b85670f54278748c5ed12a93
gracejansen227 <graceowensjansen@gmail.com>
2020-06-11 21:15:10 -0500

    Added logic for winning combinations, toggle logic corrected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { data } from '../data/data';
+import { winningPositions } from '../data/winningPositions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   title = 'pride-bingo';
   data = [];
   prideTitle = [];
-  isBoxChecked = false;
+  isBoxChecked = [];
+  winningPositions = [];
 
   ngOnInit() {
-    this.data = Array.from(Array(25).keys());
-    this.prideTitle = ('PRIDE').split('');
+    this.data = data;
+    this.prideTitle = 'PRIDE'.split('');
+    this.winningPositions = winningPositions;
   }
 
-  // checkBox(){
-  //   // fix this to make a toggle
-  //   this.isBoxChecked = !this.isBoxChecked;
-  // }
+  checkBox(i) {
+    // fix this to make a toggle
+    this.isBoxChecked[i] = this.isBoxChecked[i] || false;
+    this.isBoxChecked[i] = !this.isBoxChecked[i];
+
+    const winning = this.checkIfWon(this.isBoxChecked);
+    console.log('winnininiggg', winning);
+
+    // do check if won function here
+  }
+
+  checkIfWon(arrayOfChecked) {
+    const winningGames = this.winningPositions.map((gameType) => {
+      const positions = gameType.positions;
+      return positions.every((position) => arrayOfChecked[position] === true);
+    });
+
+    if (winningGames.includes(true)) {
+      return true;
+    }
+    return false;
+  }
 }
---
+++ src/data/winningPositions.ts
@@ -0,0 +1,11 @@
+const diagonal1 = {
+  name: 'Diagonal',
+  positions: [4, 8, 12, 16, 20],
+};
+
+const diagonal2 = {
+  name: 'Diagonal',
+  positions: [0, 6, 12, 18, 24],
+};
+
+export const winningPositions = [diagonal1, diagonal2];
